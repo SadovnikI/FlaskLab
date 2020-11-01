26a82e3c69b97167a419128f127ba9d86dd24ddf
SadovnikI <illia.sadovnik.knm.2019@lpnu.ua>
2020-11-01 16:13:31 +0200

    Add existing file

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/api/v1/hello-world-<username>")
+def hello_world(username):
+    return 'Hello World %s !' % username
+
+if __name__ == '__main__':
+    app.run()
---
+++ yourapp.wsgi
@@ -0,0 +1,6 @@
+
+#import sys
+#sys.path.insert(0, 'C:\\Users\\sadov\\laba4pp')
+
+
+
